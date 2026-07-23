94dc2d0a7e7d3a29f54ca727471833ffe22d8d21
Ryan Thomas <ryanthomasj15@gmail.com>
2026-07-23 14:56:54 +0100

    correct fool effect

--- Assets/Scripts/Card/MajorArcana.cs
+++ Assets/Scripts/Card/MajorArcana.cs
@@ -17,15 +17,28 @@ public class Fool : MajorArcana
     public Fool(CardSO cardSO) : base(cardSO)
     {
         Name = "The Fool";
-        FateCost = 1;
+        FateCost = 10;
     }
 
     public override IEnumerator ExecuteEffect()
     {
+        while (GameManager.Instance.PlayArea.Cards.Count > 0)
+        {
+            yield return GameManager.Actions.ExecuteImmediate(new ChangeZone(GameManager.Instance.PlayArea.Cards[0], GameManager.Instance.Deck));
+        }
+
+        while (GameManager.Instance.DiscardPile.Cards.Count > 0)
+        {
+            yield return GameManager.Actions.ExecuteImmediate(new ChangeZone(GameManager.Instance.DiscardPile.Cards[0], GameManager.Instance.Deck));
+        }
+
+        while (GameManager.Instance.Hand.Cards.Count > 0)
+        {
+            yield return GameManager.Actions.ExecuteImmediate(new ChangeZone(GameManager.Instance.Hand.Cards[0], GameManager.Instance.Deck));
+        }
+
         GameManager.Instance.Deck.Shuffle();
         GameManager.Instance.Deck.UpdateVisuals();
-
-        return null;
     }
 }
 
