e53f6be381d6a9ba947df4c254ba7c905ab927bb
Ryan Thomas <ryanthomasj15@gmail.com>
2026-07-24 12:04:37 +0100

    implement emperor, empress, tower

--- Assets/Scripts/Card/MajorArcana/TheEmperor.cs
+++ Assets/Scripts/Card/MajorArcana/TheEmperor.cs
@@ -8,9 +8,19 @@ public class TheEmperor : MajorArcana
     {
         Name = "The Emperor";
         FateCost = 10;
-        Text = "Reduce your turns by the number of Death cards you currently have in your deck.";
+        Text = "Skip a number of turns equal to your Doom";
     }
 
     public override IEnumerator ExecuteEffect()
-    { return null; }
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < GameManager.Instance.Doom - 1; i++)
+        {
+            GameManager.Instance.DecrementTurn();
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        yield return new WaitForSeconds(0.25f);
+    }
 }
